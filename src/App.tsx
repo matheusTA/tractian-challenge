753563c3ddd53642eeb494f371c4f16ec46783c4
matheusTA <matrajano12@gmail.com>
2025-02-18 00:26:19 -0300

    feat: refactor App component layout, integrate react-query, and remove unused pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/react-query";
-import { AppRouter } from "./routes";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { AssetFilterSelect } from "./components/asset-filter-select";
+import { Header } from "./components/header";
+import { GripVertical } from "lucide-react";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-    </QueryClientProvider>
+    <div className="flex flex-col h-screen w-full overflow-hidden">
+      <Header />
+
+      <div className="flex flex-1 flex-col p-4 pt-0 overflow-hidden">
+        <AssetFilterSelect />
+
+        <div className="flex flex-1 overflow-hidden">
+          <PanelGroup direction="horizontal" className="">
+            <Panel
+              className="border border-zinc-100 rounded-sm"
+              order={1}
+              defaultSize={25}
+              minSize={15}
+            >
+              <div>Assets list</div>
+            </Panel>
+
+            <PanelResizeHandle className="flex w-3 items-center justify-center">
+              <GripVertical />
+            </PanelResizeHandle>
+
+            <Panel
+              className="border border-zinc-100 rounded-sm"
+              order={2}
+              minSize={50}
+            >
+              <div className="">Asset details</div>
+            </Panel>
+          </PanelGroup>
+        </div>
+      </div>
+    </div>
   );
 }
 
