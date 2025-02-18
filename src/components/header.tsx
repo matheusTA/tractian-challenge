f028d57db3eeb0596b5fa56e33d2c14203bf763c
matheusTA <matrajano12@gmail.com>
2025-02-18 19:06:27 -0300

    feat: add company selection reset functionality in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,22 @@ import logo from "../assets/logo.svg";
 import { useCompanyQuery } from "../hooks/use-company-query";
 import { useComapnySelectedStore } from "../store/company-selected";
 import { Button } from "./ui/button";
+import { Company } from "../shared/schemas/company";
+import { useAssetTreeSelectedStore } from "../store/asset-tree-selected";
 
 export function Header() {
   const { companySelected, setCompanySelected: setCompany } =
     useComapnySelectedStore();
+  const { resetAssetTreeSelected } = useAssetTreeSelectedStore();
   const { data } = useCompanyQuery();
 
   function isSelected(companyId: string) {
     return companySelected?.id === companyId;
+  }
+
+  function handleSelectCompany(company: Company) {
+    resetAssetTreeSelected();
+    setCompany(company);
   }
 
   return (
@@ -22,7 +30,7 @@ export function Header() {
           <Button
             key={company.id}
             variant={isSelected(company.id) ? "selected" : "default"}
-            onClick={() => setCompany(company)}
+            onClick={() => handleSelectCompany(company)}
           >
             <Building2 /> {company.name}
           </Button>
--- src/store/asset-tree-selected.ts
+++ src/store/asset-tree-selected.ts
@@ -4,11 +4,13 @@ import { AssetTree } from "../shared/types/asset-tree";
 interface AssetTreeSeletedState {
   assetTreeSelected: AssetTree | null;
   setAssetTreeSelected: (assetTree: AssetTree) => void;
+  resetAssetTreeSelected: () => void;
 }
 
 export const useAssetTreeSelectedStore = create<AssetTreeSeletedState>()(
   (set) => ({
     assetTreeSelected: null,
     setAssetTreeSelected: (assetTree) => set({ assetTreeSelected: assetTree }),
+    resetAssetTreeSelected: () => set({ assetTreeSelected: null }),
   })
 );
